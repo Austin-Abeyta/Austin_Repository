99e135444be6df33abc0be2180906d7b6ff700cf
Austin Abeyta <austin.abeyta@gmail.com>
2018-02-25 23:10:30 -0800

    Added_Files

---
+++ 03_linreg_starter.py
@@ -0,0 +1,92 @@
+""" Starter code for simple linear regression example using placeholders
+Created by Chip Huyen (huyenn@cs.stanford.edu)
+CS20: "TensorFlow for Deep Learning Research"
+cs20.stanford.edu
+Lecture 03
+"""
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import xlrd
+import utils
+
+cwd = os.getcwd()
+print(cwd)
+DATA_FILE = '/Users/austinabeyta/Desktop/stanford-tensorflow-tutorials-master/2017/examples/data/fire_theft.xls'
+
+# Step 1: read in data from the .xls file
+
+book = xlrd.open_workbook(DATA_FILE, encoding_override = "utf-8") 
+sheet = book.sheet_by_index(0) #sheet <xlrd.sheet.Sheer object at 0x11c715e10
+data = np.asarray([sheet.row_values(i) for i in range(1, sheet.nrows)])
+n_samples = sheet.nrows -1 #42
+# Step 2: create placeholders for X (birth rate) and Y (life expectancy)
+# Remember both X and Y are scalars with type float
+X, Y = None, None
+X = tf.placeholder(tf.float32, name = 'X')
+Y = tf.placeholder(tf.float32, name = 'Y')
+
+# Step 3: create weight and bias, initialized to 0.0
+# Make sure to use tf.get_variable
+w = tf.Variable(0.0, name ='weights')
+b = tf.Variable(0.0, name = 'bias')
+
+# Step 4: build model to predict Y
+# e.g. how would you derive at Y_predicted given X, w, and b
+Y_predicted = X * w + b
+
+# Step 5: use the square error as the loss function
+loss = None
+loss = tf.square(Y - Y_predicted, name = 'loss')
+# Step 6: using gradient descent with learning rate of 0.001 to minimize loss
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
+
+start = time.time()
+
+# Create a filewriter to write the model's graph to TensorBoard
+#############################
+########## TO DO ############
+#############################
+
+with tf.Session() as sess:
+    # Step 7: initialize the necessary variables, in this case, w and b
+    sess.run(tf.global_variables_initializer())
+    
+    writer = tf.summary.FileWriter('./my_graph/03/linear_reg',sess.graph)
+
+    # Step 8: train the model for 100 epochs
+    for i in range(100):
+        total_loss = 0
+        for x, y in data:
+            # Execute train_op and get the value of loss.
+            # Don't forget to feed in data for placeholders
+            _, l = sess.run([optimizer,loss], feed_dict={X: x, Y: y})
+            total_loss += l
+
+        print('Epoch {0}: {1}'.format(i, total_loss/n_samples))
+
+    # close the writer when you're done using it
+    #############################
+    ########## TO DO ############
+    #############################
+    writer.close()
+    
+    # Step 9: output the values of w and b
+    w_value, b_value = sess.run([w,b])
+
+print('Took: %f seconds' %(time.time() - start))
+
+X,Y = data.T[0],data.T[1] 
+plt.plot(X,Y, 'bo', label = 'Real data')
+plt.plot(X,X* w_value + b_value, 'r', label = 'Predicted data')
+plt.legend()
+plt.show()
+# uncomment the following lines to see the plot 
+# plt.plot(data[:,0], data[:,1], 'bo', label='Real data')
+# plt.plot(data[:,0], data[:,0] * w_out + b_out, 'r', label='Predicted data')
+# plt.legend()
+# plt.show()
